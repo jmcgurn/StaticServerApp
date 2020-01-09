620929266d092382b2a7a39b73f4c24eaccda623
McGurn III <jmcgurn@labs.neit.edu>
2020-01-09 13:49:29 -0500

    First Setup For static express app

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var router = express.Router();
+
+//Setup for first route
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname+"/Views/index.html"));
+})
+
+/*app.get('about', function(req, res){
+    res.sendFile(path.join(__dirname+"/Views/about.html"));
+})
+
+app.get('contact', function(req, res){
+    res.sendFile(path.join(__dirname+"/Views/contact.html"));
+})
+*/
+app.use(express.static(__dirname+'/Views'));
+
+app.use('/', router);
+
+
+app.listen(3000, function(){
+console.log("Server is running on port 3000")
+});
